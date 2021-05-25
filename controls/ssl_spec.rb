625b6cb0ab87b698b249996fa1882fe5ef1c5855
Lance Albertson <lance@osuosl.org>
2021-05-24 17:11:22 -0700

    Ensure we check for all 3DES / CBC ciphers
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- controls/ssl_spec.rb
+++ controls/ssl_spec.rb
@@ -2,7 +2,7 @@ port = input('ssl_port')
 host = input('ssl_host')
 
 control 'ssl-baseline' do
-  describe ssl(port: port, host: host).ciphers('TLS_RSA_WITH_3DES_EDE_CBC_SHA') do
+  describe ssl(port: port, host: host).ciphers(/3DES_EDE_CBC_SHA/) do
     it { should_not be_enabled }
   end
 
