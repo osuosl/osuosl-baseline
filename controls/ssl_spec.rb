4456f3b11fe6b50a726186c473add2c690bc9f2e
Lance Albertson <lance@osuosl.org>
2021-05-24 18:16:50 -0700

    Ensure RC4 is disabled
    Also switch to using an Array for ssl_port so we can check more than one port on
    a host.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- controls/ssl_spec.rb
+++ controls/ssl_spec.rb
@@ -1,14 +1,20 @@
-port = input('ssl_port')
+ports = input('ssl_port')
 host = input('ssl_host')
 
 control 'ssl-baseline' do
-  describe ssl(port: port, host: host).ciphers(/3DES_EDE_CBC_SHA/) do
-    it { should_not be_enabled }
-  end
-
-  %w(ssl2 ssl3 tls1.0 tls1.1).each do |p|
-    describe ssl(port: port, host: host).protocols(p) do
+  ports.each do |port|
+    describe ssl(port: port, host: host).ciphers(/WITH_3DES/) do
       it { should_not be_enabled }
+    end
+
+    describe ssl(port: port, host: host).ciphers(/WITH_RC4/) do
+      it { should_not be_enabled }
+    end
+
+    %w(ssl2 ssl3 tls1.0 tls1.1).each do |p|
+      describe ssl(port: port, host: host).protocols(p) do
+        it { should_not be_enabled }
+      end
     end
   end
 end
